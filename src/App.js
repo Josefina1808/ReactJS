bc638f6836b1343803a2830b3d16eb7e626a6702
Josefina Esnaola Borbore <josefinaesna@gmail.com>
2021-08-01 15:11:14 -0300

    Menú e-commerce

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-import {navBar} from './components/header/navBar'
+import { NavBar } from './components/NavBar/navBar';
 
 function App() {
   return (
     <div className="App">
-      <navBar />
+      <NavBar/>
     </div>
   );
 }
